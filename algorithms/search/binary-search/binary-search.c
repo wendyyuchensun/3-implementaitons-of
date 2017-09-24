801b3d4fb3c7454d83d388bbe738fd1e046d1add
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2017-09-24 19:39:56 +0800

    Add binary-search.c

---
+++ algorithms/search/binary-search/binary-search.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int binary_search (int sorted_arr[], int length, int target) {
+  int first = 0;
+  int last = length - 1;
+
+  while (first <= last) {
+    int middle = (last - first) / 2 + first;
+    int middle_elm = sorted_arr[middle];
+
+    if (middle_elm == target) {
+      return middle_elm;
+    }
+    else if (middle_elm > target) {
+      last = middle - 1;
+    }
+    else if (middle_elm < target) {
+      first = middle + 1;
+    }
+  }
+
+  return -1;
+}
