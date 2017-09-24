6eba7f403087f7f89fe20b2459bb70b8a06cf6d5
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2017-09-24 21:22:48 +0800

    Add selectionSort.js

---
+++ algorithms/sorting/selection-sort/selectionSort.js
@@ -0,0 +1,13 @@
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) min = j
+    }
+
+    if (min !== i) [arr[i], arr[min]] = [arr[min], arr[i]]
+  }
+
+  return arr
+}
