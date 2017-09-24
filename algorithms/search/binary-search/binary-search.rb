7ee55217a651e840d38df0df132736437fa6e971
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2017-09-24 19:29:53 +0800

    Use while in binary-search.rb instead of recursion

--- algorithms/search/binary-search/binary-search.rb
+++ algorithms/search/binary-search/binary-search.rb
@@ -1,18 +1,17 @@
 def binary_search sorted_arr, target
-  return nil if sorted_arr.empty?
+  first = 0
+  last = sorted_arr.length - 1
 
-  middle_indx = (sorted_arr.length / 2).floor
-  middle_elm = sorted_arr[middle_indx]
+  while first <= last
+    middle = ((last - first) / 2 + first).floor
+    middle_elm = sorted_arr[middle]
 
-  if middle_elm == target
-    middle_elm
-  elsif sorted_arr.length > 1
-    if middle_elm > target
-      binary_search sorted_arr.take(middle_indx), target
+    if middle_elm == target
+      break middle_elm
+    elsif middle_elm > target
+      last = middle - 1
     elsif middle_elm < target
-      binary_search sorted_arr.drop(middle_indx + 1), target
+      first = middle + 1
     end
-  else
-    nil
   end
 end
